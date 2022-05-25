073f3b4484a33a98a7965f06d59fd3ceeeb53a95
Maicol <michael.negrete16@gmail.com>
2022-05-25 16:50:15 -0500

    Creacion del calendario con big-Calendar y moment

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
+
 import Navbar from '../ui/Navbar'
+
+const localizer = momentLocalizer(moment)
+const events = [{
+  title: 'cumpleaños del jefe',
+  start: moment().toDate(),
+  end: moment().add(2, 'hour').toDate(),
+  bgcolor: '#fafafa'
+}]
 
 const CalendarScreen = () => {
   return (
-    <div>
+    <div className='calendar-screen'>
         <Navbar/>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+        />
     </div>
   )
 }
