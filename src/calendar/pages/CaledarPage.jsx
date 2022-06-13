6c37300e903cac0f5a4f02255f7680800c9a4024
Maicol <michael.negrete16@gmail.com>
2022-06-13 11:18:31 -0500

    Rutas de la aplicacion

---
+++ src/calendar/pages/CaledarPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CaledarPage = () => {
+  return (
+    <div>CaledarPage</div>
+  )
+}
+
+export default CaledarPage
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import LoginPage from '../auth/pages/LoginPage'
+import CaledarPage from '../calendar/pages/CaledarPage'
+
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+const AppRouter = () => {
+  
+    const authStatus = 'not-authenticated'
+
+    return (
+        <Routes>
+            { 
+                (authStatus === 'not-authenticated')
+                 ? <Route path='/auth/*' element={<LoginPage/>}/>
+                 : <Route path='/*' element={<CaledarPage/>}/> 
+            }
+
+            <Route path='/*' element={<Navigate to='/auth/login'/>} />
+        </Routes>
+    )
+}
+
+export default AppRouter
