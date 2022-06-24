0662827b4f9030e640893d81f2b987e10396bdb6
Maicol <michael.negrete16@gmail.com>
2022-06-24 09:35:37 -0500

    AuthSlice (reducer) Creado

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+    name:'auth',
+    initialState:{
+        // El status sera para saber si la persona esta autenticada o no
+        status: 'checking',  // 'authenticated' , 'not-authenticated'
+        user: {},
+        errrorMessage: undefined
+    },
+    reducers:{
+
+        onChecking: (state) => {
+            state.status = 'checking'
+            state.user = {}
+            state.errrorMessage = undefined
+        },
+        
+        onLogin: (state,{payload}) => {
+            state.status = 'authenticated',
+            state.user = payload,
+            state.errrorMessage = undefined
+        },
+
+        
+
+    }
+})
+
+export const {onChecking,onLogin} = authSlice.actions
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+// Store
 import { uiSlice } from "./ui/uiSlice";
 import { calendarSlice } from "./calendar/calendarSlice";
+import { authSlice } from "./auth/authSlice";
 
 export const store = configureStore({
     reducer: {
+        auth: authSlice.reducer,
         calendar: calendarSlice.reducer,
-        ui: uiSlice.reducer
+        ui: uiSlice.reducer,
     },
     middleware: (getDefaultMiddleware)=> getDefaultMiddleware({
         serializableCheck: false
