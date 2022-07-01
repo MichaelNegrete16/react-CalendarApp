db6be0d52e1a908b2d66dd8022290c15d1b34ea8
Maicol <michael.negrete16@gmail.com>
2022-07-01 08:57:43 -0500

    Eliminar un evento

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -39,9 +39,14 @@ export const useCalendarStore = () => {
         
     }
 
-    const startDeleteEvent = () => {
-        // TODO: llegar al backend
-        dispatch(onDeleteEvent())
+    const startDeleteEvent = async () => {
+        try {
+            await calendarApi.delete(`/events/${activeEvent.id}`)
+            dispatch(onDeleteEvent())
+        } catch (error) {
+            console.log(error)
+            Swal.fire('Error al eliminar',error.response.data.msg,'error')
+        }
     }
 
     // Cargar los enventos del backend
